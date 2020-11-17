f36a289682eda6cd445d7dbb94b7e8ca9cdc8f9e
giannishorgos <giannis.21.osfp@gmail.com>
2020-11-17 17:15:47 +0200

    updated

--- Programm.py
+++ Programm.py
@@ -1,5 +1,6 @@
 from datetime import *
 import time
+import os
 
 class Program:
     def __init__(self, username):
@@ -16,7 +17,7 @@ class Program:
         for i in range(5):
             flag = True
             print(dic[i])
-            while flag:      
+            while flag:     
                 choice = str(input("Do you want to add something? [Y/N]"))
                 if choice == 'N' or choice == 'n':
                     flag = False
@@ -24,24 +25,25 @@ class Program:
                     lecture.append(str(input("LECTURE: ")))
                     hour.append(str(input("Begins at: ")))
                     link.append(str(input("The link for the call is: ")))
-                    Program.makeTxtFile(i, lecture, hour, link)
+                    self.makeTxtFile(i, lecture, hour, link)
                     lecture.clear()
                     hour.clear()
                     link.clear()
                     
-    @staticmethod
-    def makeTxtFile(day, lecture, hour, link):
-        p = open("{self.username}.txt", "a") 
+    def makeTxtFile(self, day, lecture, hour, link):
+        currentDir = os.getcwd()
+        p = open(currentDir + "/" + self.username + ".txt", "a") 
 
         for i in range(len(hour)):
-            p.write(day + " ")
-            p.write(hour[i] + "    ")
-            p.write(lecture[i] + "    ")
+            p.write(str(day) + " ")
+            p.write(hour[i] + " ")
+            p.write(lecture[i] + " ")
             p.write(link[i] + "\n")
         p.close()
 
     def theRightLink(self):
-        txtFile = open("/home/giannis/Documents/Codes/Zoom Calls Automation/pl.txt", "r")
+        currentDir = os.getcwd()
+        txtFile = open(currentDir + "/" + self.username + ".txt", "r")
         flag = True
 
         i = 0
@@ -56,8 +58,7 @@ class Program:
                 line = txtFile.readline()
             
             if int(line[0]) == day:
-                if abs(int(line[2:4]) -  hour) <= 1 and abs(int(line[5:7]) - minutes) <= 1:
+                if abs(int(line[2:4]) -  hour) <= 1 and abs(int(line[5:7]) - minutes) <= 20:
                     flag = False
                     begins = line.find("https")
                     return line[begins : len(line)]
-            time.sleep(30)
