586b7cb5383ab689d4b28502e5794077b024be4f
giannishorgos <giannis.21.osfp@gmail.com>
2020-11-17 19:38:36 +0200

    Updated

--- Programm.py
+++ Programm.py
@@ -3,36 +3,20 @@ import time
 import os
 
 class Program:
-    def __init__(self, username):
+    def __init__(self, username=None):
         self.username = username
-
-    def getProgram(self):
-        #mera, wra, mathhma kai link
-        hour = []
-        lecture = []
-        link = []
-
-        dic = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"]
-
-        for i in range(5):
-            flag = True
-            print(dic[i])
-            while flag:     
-                choice = str(input("Do you want to add something? [Y/N]"))
-                if choice == 'N' or choice == 'n':
-                    flag = False
-                else:
-                    lecture.append(str(input("LECTURE: ")))
-                    hour.append(str(input("Begins at: ")))
-                    link.append(str(input("The link for the call is: ")))
-                    self.makeTxtFile(i, lecture, hour, link)
-                    lecture.clear()
-                    hour.clear()
-                    link.clear()
+    
+    def setUsername(self, username):
+        self.username = username
                     
     def makeTxtFile(self, day, lecture, hour, link):
         currentDir = os.getcwd()
-        p = open(currentDir + "/" + self.username + ".txt", "a") 
+        windows = "\\"
+        linux = "/"
+        if linux in currentDir:
+            p = open(currentDir + linux + self.username + ".txt", "a") 
+        else:
+            p = open(currentDir + windows + self.username + ".txt", "a")
 
         for i in range(len(hour)):
             p.write(str(day) + " ")
@@ -54,11 +38,13 @@ class Program:
             line = txtFile.readline()
             if len(line) == 0:
                 txtFile.close()
-                txtFile = open("/home/giannis/Documents/Codes/Zoom Calls Automation/pl.txt", "r")
+                txtFile = open(currentDir + "/" + self.username + ".txt", "r")
                 line = txtFile.readline()
             
             if int(line[0]) == day:
-                if abs(int(line[2:4]) -  hour) <= 1 and abs(int(line[5:7]) - minutes) <= 20:
+                thour = (int(line[2:4]) -  hour)*60
+                tminutes =  int(line[5:7]) - minutes + thour
+                if tminutes <= 30 and abs((int(line[2:4]) -  hour)) <= 1:
                     flag = False
                     begins = line.find("https")
                     return line[begins : len(line)]
